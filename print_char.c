42eb06408d14df6dd234c6fbe99642f9197d2e64
nadaMAoui <maaouinada747@gmail.com>
2022-11-09 18:28:07 +0100

    update

--- print_char.c
+++ print_char.c
@@ -2,12 +2,12 @@
 /**
  * funct_char - a function that prints a character 
  * @arg: the list of arguments that prints is recieving
- * Return: return 0 if ch is a character else 1
+ * Return: return 1 if ch is a character else 0
 */
 int funct_char (va_list arg)
 {
 char ch; 
 ch = (va_arg(arg, int));
 _putchar(ch);
-return(0);
+return(1);
 }
--- print_string.c
+++ print_string.c
@@ -3,16 +3,16 @@
  * print_string - prints strings
  * @arg: list of arguments the function prints is recieving
  * Return: return the length of the string
-*/
+ */
 int funct_string(va_list arg)
 {
-unsigned int i;
-char *str;
+    unsigned int i;
+    char *str;
 
-str = va_arg(arg, char*);
-if (str == 0)
-str = "(null)";
-for (i = 0; str[i] != 0; i++)
-_putchar(str[i]);
-return(i);
+    str = va_arg(arg, char *);
+    if (str == 0)
+        str = "(null)";
+    for (i = 0; str[i] != 0; i++)
+        _putchar(str[i]);
+    return (i);
 }
