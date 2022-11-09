9cf33b7cc8bcccf9977093e0cfaf45d8c8b916a5
nadaMAoui <maaouinada747@gmail.com>
2022-11-09 18:34:16 +0100

    update

--- funct_percent.c
+++ funct_percent.c
@@ -1,11 +1,11 @@
-#include"main.h"
+#include "main.h"
 /**
  * funct_percent - a function that prints the percent sign
  * @arg: list of arguments the function prints is recieving
- * Return: return 0 (success)
+ * Return: return 1 (success)
 */
 int funct_percent(__attribute__((unused)) va_list arg)
 {
     _putchar('%');
-    return(0);
+    return(1);
 }
